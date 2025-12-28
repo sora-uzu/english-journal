c4eb46060d23c191589757bc1d81a0a9fee97e68
sora_uzu <naruto07170717@gmail.com>
2025-12-28 19:12:12 +0900

    feat: update app icons and manifest for improved PWA support; replace old apple-touch-icon and add new icons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="theme-color" content="#7C3AED">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon-180.png">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
