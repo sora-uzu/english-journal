57eed2d74929316d694700bb6f58da8270ff2768
sora_uzu <naruto07170717@gmail.com>
2026-01-16 17:32:16 +0900

    feat: Add CSRF token validation exception for guest feedback API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/guest/feedback',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
